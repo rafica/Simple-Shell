10c89ad243bb2dc292781fc5ea8cc37f88d878fd
rafica <ra2688@columbia.edu>
2014-09-08 20:40:44 -0400

    first part

--- basic.c
+++ basic.c
@@ -1,46 +1,109 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<errno.h>
+#include<unistd.h>
+#include<string.h>
+
+char* get_input();
+
+extern int errno;
 
 int main(){
 	
-	unsigned int len_max = 128;
-	unsigned int current_size = 0;
-	
-	char *ptr = malloc(len_max);
-	current_size = len_max;
+	char *command;
 
-	char next;
 
-	int max_commands = 1;
+	int max_commands = 10;
 	int y =0;
+	char *token;
+	char *arg[100];
+	int i=0;
+
 	while(y<max_commands){
 		y++;
 		//display $
 		printf("$");
 
 
-		if(ptr!= NULL){
-			unsigned int i=0;
-			// get input
-			while((next=getchar())!=EOF && next!='\n'){
-				ptr[i++] = next;
-
-				if(i == current_size){
-					current_size = i+len_max;
-					ptr = realloc(ptr, current_size);
-				}
-			}
-			ptr[i]='\0';
-
-			printf("\n Long string value :%s \n\n", ptr);
-
-			free(ptr);
-			ptr = NULL;
+		command = get_input();
+		
+		token = strtok(command, " \t");
+		while(token != NULL){
+			arg[i++] = token;
+			token = strtok(NULL, " \t");
+		
+		}
 
 
-		}	
+		if(strcmp(arg[0], "exit")==0)
+			exit(0);
 
+		
+		
+	        //after using the command
+		if(command)
+			free(command);
+		command = NULL;
+
+		
+	}
+	return 0;
+}
+
+
+char* get_input(){
+
+
+	unsigned int len_max = 128;
+	unsigned int current_size = 0;
+	
+
+	char *ptr;
+	char *tmp = malloc(len_max);
+	if(tmp){
+		ptr = tmp;
+		tmp = NULL;
+	}
+	else{
+		//fprintf(stderr, "Error allocating space :%s", strerror(errno));
+		printf("Error : %s", strerror(errno));
+		return NULL;	
 	}
 
-	return 0;
+
+	current_size = len_max;
+
+	char next;
+		
+	unsigned int i=0;
+
+
+	while((next=getchar())!=EOF && next!='\n'){
+			ptr[i++] = next;
+
+			if(i == current_size){
+				current_size = i+len_max;
+				
+				tmp = realloc(ptr, current_size);
+				if(tmp){
+					ptr = tmp;
+					tmp = NULL;
+				}
+				else{
+					printf("Error : %s\n", strerror(errno));
+					//flushing the input
+					while((next=getchar())!='\n' && next!= EOF);
+
+					free(ptr);
+					ptr = NULL;
+					return NULL;	
+				}
+			}
+	}
+	ptr[i]='\0';
+
+
+	return ptr;
+
+
 }
