d408331e4037908f166f1f947e8fe1c92b96c9bd
rafica <ra2688@columbia.edu>
2014-09-10 03:33:47 -0400

    path +

--- basic.c
+++ basic.c
@@ -3,22 +3,26 @@
 #include<errno.h>
 #include<unistd.h>
 #include<string.h>
+#include<stdbool.h>
 
 char* get_input();
+bool process_string(char* str[], int i);
 
 extern int errno;
+char *path="";
 
 int main(){
 	
 	char *command;
 
+//	path = "";
 
 	int max_commands = 10;
 	int y =0;
 	char *token;
 	char *arg[100];
 	int i=0;
-	char *directory;
+	bool carryOn;
 
 	while(y<max_commands){
 		y++;
@@ -35,32 +39,81 @@ int main(){
 		
 		}
 
-
-		if(strcmp(arg[0], "exit")==0){
-			exit(0);
-		}
-		else if(strcmp(arg[0], "cd")==0){
-			directory = arg[1];
-			if(chdir(directory)<0){
-				printf("error: %s\n", strerror(errno));
-			}
-			
-		}
-
-
-		
-		
-		
+		carryOn = process_string(arg,i);	
+		i=0;	
 	        //after using the command
 		if(command)
 			free(command);
 		command = NULL;
 
+		if(!carryOn)
+			break;
 		
 	}
 	return 0;
 }
 
+char* concat(char *s1, char *s2){
+	char *result;
+	char *tmp = malloc(strlen(s1) + strlen(s2) + 2);
+
+	if(tmp==NULL){
+		return NULL;
+	}
+	else{
+		result = tmp;
+		strcpy(result, s1);		
+		if(strcmp(s1, "")!=0){
+			free(s1);
+			strcat(result, ":");
+		}
+		strcat(result, s2);
+
+		return result;
+	}
+
+}
+
+
+bool process_string(char* str[], int len){
+	
+	char *directory;
+	
+	if(len>0 && strcmp(str[0], "exit")==0){
+		return 0;
+	}
+	else if(len>1 && strcmp(str[0], "cd")==0){
+		directory = str[1];
+		printf("%s ", directory);
+		if(chdir(directory)<0){
+			printf("error: %s\n", strerror(errno));
+		}
+		
+	}
+	else if(len>0 && strcmp(str[0], "path")==0){
+		if(len==1){		
+			printf("%s\n", path);
+		}
+		else if(len==3){
+			if(strcmp(str[1], "+")==0){
+				path = concat(path, str[2]);
+				if(path==NULL){
+					printf("Error:%s\n ",strerror(errno));
+					return 0;
+				}
+			}
+			else if(strcmp(str[1], "-")==0){
+			//do this
+				
+			}
+		}
+		
+	}	
+
+	return 1;
+
+
+}
 
 char* get_input(){
 
